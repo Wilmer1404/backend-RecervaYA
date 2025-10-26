089072000b7d2971a633a87d3091deff302cfc70
Wilmer1404 <samirloroamaya24@gmail.com>
2025-10-26 16:47:11 -0500

    GAAAA

--- src/main/java/com/reservaya/reservaya_api/config/JwtAuthenticationFilter.java
+++ src/main/java/com/reservaya/reservaya_api/config/JwtAuthenticationFilter.java
@@ -18,7 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor // Lombok: crea un constructor con los campos final
+@RequiredArgsConstructor 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
